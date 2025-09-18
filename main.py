5220248d003272685ae77c3712582f1b15e19816
zzzpz <1911293650@qq.com>
2025-09-18 14:00:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-num = input()
-if len(num) != 5 or not num.isdigit():
-    print("输入错误：请输入5位数字")
-else:
+def main():
+    num = input().strip()
+    
+    if not num.isdigit() or len(num) != 5:
+        print("输入错误：请输入5位数字")
+        return
+        
     if num == num[::-1]:
         print("是回文数")
     else:
         print("不是回文数")
+
+if __name__ == "__main__":
+    main()
