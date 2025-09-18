09774d37078f5867c23ec69240317932ace8dbb7
zzzpz <1911293650@qq.com>
2025-09-18 14:02:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     num = input().strip()
     
     if not num.isdigit() or len(num) != 5:
-        print("输入错误：请输入5位数字")
+        print("不是回文数")
         return
         
     if num == num[::-1]:
